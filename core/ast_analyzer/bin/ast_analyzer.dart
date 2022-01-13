2338801fd62701c205c5dda0efbf6575eb00dbc5
Kenneth <kenneth.ng.5226@outlook.com>
2022-01-13 17:40:13 +0000

    Create Dart analyzer template

---
+++ core/ast_analyzer/bin/ast_analyzer.dart
@@ -0,0 +1,5 @@
+import 'package:ast_analyzer/ast_analyzer.dart' as ast_analyzer;
+
+void main(List<String> arguments) {
+  print('test');
+}
---
+++ core/ast_analyzer/test/ast_analyzer_test.dart
@@ -0,0 +1,8 @@
+import 'package:ast_analyzer/ast_analyzer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
